3e11cfecdc2ba79f3b7c48e15195498e02bbb270
Seele0709 <yu07090618@gmail.com>
2022-07-29 11:34:35 +0900

    first changed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    body::BoxBody,
+    dev::ServiceResponse,
+    error,
+    http::{header::ContentType, StatusCode},
+    middleware::{self, ErrorHandlerResponse, ErrorHandlers},
+    web, App, Error, HttpResponse, HttpServer, Result, Responder,get, post
+};
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Deserialize)]
+struct InputTag {
+    tag: String,
+}
+
+#[derive(Deserialize)]
+struct UserResponse{
+    lat: String,
+    lon: String,
+    date: String,
+    url: String,
+}
+
+#[get("/")]
+async fn get_tag(
+    query: web::Query<InputTag>//tagの呼び出しはquery.tag;で
+) -> Result<HttpResponse, Error> {
+
+    Ok(HttpResponse::Ok().body(format!("tag is {}", tag)))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {    
+        App::new()
+            .service(get_tag)
+    })
+    // ローカルホストのport8080で起動
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
